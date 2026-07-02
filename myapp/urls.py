c0d127e459c70124efce7cd91da965ad0a24f47b
Parul Gupta <parulgupta31@gmail.com>
2026-07-02 21:49:48 +0530

    Update project

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.navigation, name='navigation'),
-    path('guess/', views.guessno, name='guessno'),
+    path('userguess/', views.guessno, name='guessno'),
     path('rps/',views.home, name='home'),
     path('cricket/',views.cricket, name='cricket'),
     path('cricketrules/',views.crules,name='crules'),
+    path('trivia/',views.trivia,name='trivia'),
+    path('computerguesses/',views.computerGuesses,name='computerGuesses'),
+    path('guess/',views.gnav,name='gnav')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,5 @@
 import random
+import time
 from django.shortcuts import render
 
 def home(request):
@@ -50,7 +51,6 @@ def home(request):
         't': request.session["nofg"]-(request.session["w"]+request.session["l"]),
             }
     return render(request, 'myapp/home.html', context)
-
 def guessno(request):
     result=None
     userchoice=None
@@ -207,3 +207,123 @@ def cricket(request):
     return render(request,'myapp/cricket.html',context)
 def crules(request):
     return render(request,'myapp/cricketrules.html',{})
+def trivia(request):
+    q=["1+1?\na.1 b.2 c.3 d.11","2+2?<br>a.4 b.2 c.12 d.22","1+2?<br>a.3 b.4 c.2 d.9"]
+    a=["b","a","a"]
+    if "p" not in request.session:
+        request.session["p"]=0
+    if "times" not in request.session:
+        request.session["times"]=0
+    question=None
+    result=None
+    mode=None
+    tim=None
+    r=None
+    h=None
+    if request.POST.get("wish")=="again":
+        request.session["p"]=0
+        request.session["times"]=0
+        request.session.pop("r",None)
+        request.session.pop("l",None)
+        request.session.pop("s",None)
+    elif request.POST.get("start")=="play" and request.session["times"]<5:
+        request.session["s"]=random.randint(1,10)
+        request.session["l"]=random.randint(0,2)
+        request.session["r"]=time.time()
+        tim="You have "+str(request.session["s"])+" seconds to answer this question"
+        question=q[request.session["l"]]
+    elif request.POST.get("option") and request.session["times"]<5:
+        h=time.time()
+        request.session["times"]+=1
+        if request.POST.get("option") == "timeout":
+            result = "Time's up!\n+0 points"
+        elif request.POST.get("option")==a[request.session["l"]]:
+            if h-request.session["r"]>request.session["s"]:
+                result="Correct!But you took too long!!!\n+0 points"
+            else:
+                request.session["p"]+=5+round(request.session["s"]+request.session["r"]-h)
+                result="Correct!\n+5 points\n+"+str(round(request.session["s"]-h+request.session["r"]))+" bonus points"
+        else:
+            result="WRONG\n+0 points"
+        if request.session["times"] < 5:
+            request.session["s"] = random.randint(1,10)
+            request.session["l"] = random.randint(0,2)
+            request.session["r"] = time.time()
+            tim = "You have "+str(request.session["s"])+" seconds to answer this question"
+            question = q[request.session["l"]]
+        else:
+            request.session.pop("r",None)
+            request.session.pop("l",None)
+            request.session.pop("s",None)
+    elif "l" in request.session and request.session["times"]<5:
+        tim="You have "+str(request.session["s"])+" seconds to answer this question"
+        question=q[request.session["l"]]
+    mode=request.session["times"]<5
+    context={"time":tim,"question":question,"result":result,"p":request.session["p"],"mode":mode,"s":request.session.get("s",10)}
+    return render(request,'myapp/trivia.html',context)
+def computerGuesses(request):
+    guesses=None
+    guess=None
+    mode=None
+    g=None
+    show=False
+    ges=None
+    if request.method=="GET":
+        show=True
+    if request.POST.get("wish")=="again":
+        request.session.pop("a",None)
+        request.session.pop("b",None)
+        request.session.pop("d",None)
+        request.session.pop("e",None)
+        request.session.pop("f",None)
+        request.session.pop("g",None)
+        request.session.pop("nog",None)
+        guesses=None
+        guess=None
+        show=True
+    if (request.POST.get("lrange") and request.POST.get("hrange")) or (request.POST.get("wish")=="again" and request.POST.get("lrange") and request.POST.get("hrange")):
+        show=False
+        if "a" not in request.session:
+            request.session["a"]=int(request.POST.get("lrange"))
+        if "b" not in request.session:
+            request.session["b"]=int(request.POST.get("hrange"))
+    if "a" in request.session and "b" in request.session:
+        if "d" not in request.session:
+            request.session["d"]=0
+        if (request.POST.get("lrange") and request.POST.get("hrange")):
+            if "e" not in request.session:
+                request.session["e"]=request.session["b"]-request.session["a"]
+        while "e" in request.session and request.session["e"]>1 and request.POST.get("lrange") and request.POST.get("hrange"):
+            request.session["d"]+=1
+            request.session["e"]=request.session["e"]/2
+        if "f" not in request.session:
+            request.session["f"]=0
+        if "nog" not in request.session:
+            request.session["nog"]="I'll do it in "+str(request.session["d"])+" guesses"
+        if request.POST.get("loh"):
+            if request.session["b"]-request.session["a"]>2:
+                request.session["f"]+=1
+                ges=request.session["f"]
+                if request.POST.get("loh")=="low":
+                    request.session["a"]=(request.session["b"]+request.session["a"])//2
+                elif request.POST.get("loh")=="high":
+                    request.session["b"]=(request.session["b"]+request.session["a"])//2
+                elif request.POST.get("loh")=="correct":
+                    guesses=str(request.session["f"])+" guesses"
+                    mode=False
+                if mode!=False:
+                    request.session["g"]=(request.session["b"]+request.session["a"])//2
+                    g=request.session["g"]
+                    mode=True
+            else:
+                mode=False
+                guess=str((request.session["a"]+request.session["b"])//2)+" in "+str(request.session["f"]+1)+" guesses"
+        else:
+            if "g" not in request.session:
+                request.session["g"]=(request.session["b"]+request.session["a"])//2
+            g=request.session["g"]
+            mode=True
+    context={"mode":mode,"guess":guess,"guesses":guesses,"nog":request.session.get("nog"),"g":g,"show":show,"ges":ges}
+    return render(request,'myapp/computerguesses.html',context)
+def gnav(request):
+    return render(request,'myapp/guessingpage.html',{})
